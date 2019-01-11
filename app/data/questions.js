a42f309fcbce9bae5c7311904d12bbee0e68c98e
SPallai <sangeeta.sumair@gmail.com>
2019-01-10 18:58:08 -0800

    friend finder

---
+++ app/data/questions.js
@@ -0,0 +1,16 @@
+
+function createQuestions() {
+	var q1 = "I enjoy the north pole.";
+	var q2 = "I have been on the naughty list.";
+	var q3 = "I am on the nice list.";
+	var q4 = "I can light up a room.";
+	var q5 = "I like to ride shotgun.";
+	var q6 = "I get along best in groups.";
+	var q7 = "I try to respond to texts within 3 seconds.";
+	var q8 = "I like being creative.";
+	var q9 = "I like cookies.";
+	var q10 = "I stay calm in all situations.";
+
+	var questionArray = [q1, q2, q3, q4, q5, q6, q7, q8, q9, q10];
+	return questionArray;
+}
